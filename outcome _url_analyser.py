56373ab5b6d849967f13157988a6cc1d4d522686
Domhnall <d.carlin@qub.ac.uk>
2023-06-05 18:46:35 +0100

    update url analyser

--- outcome _url_analyser.py
+++ outcome _url_analyser.py
@@ -4,61 +4,66 @@ import requests
 from urllib import parse
 from requests.exceptions import ConnectionError, TooManyRedirects, ReadTimeout
 
+"""
+Reads in the URL list. Generates a csv file of URLs and their http code when 
+attempts made to reach URL. Saves to ./responses.csv.
+"""
+
 
 def main():
-
     df = get_df_from_csv("outcomes_software_urls.csv")
-
     url_list = get_urls(df)
-
-    print(url_list)
 
     responses = []
 
     for url in url_list:
-        if url == 'missing':
-            print('missing')
+        if url == "missing":
+            print("missing")
         else:
             response = check_url(url)
             print(response[0], response[1])
             responses.append(response)
-    with open('responses.csv','w') as outfile:
+    with open("responses.csv", "w") as outfile:
         writer = csv.writer(outfile)
         writer.writerows(responses)
-        
 
 
 def get_df_from_csv(csv_to_read: str) -> pd.DataFrame:
+    """Returns dataframe from a csv
 
+    Args:
+        csv_to_read (str): filepath of csv
+
+    Returns:
+        Dataframe: df
+    """
     with open(csv_to_read) as data_in:
-        df = pd.read_csv(data_in).fillna('missing')
+        df = pd.read_csv(data_in).fillna("missing")
 
     return df
 
 
-def get_urls(df_in:pd.DataFrame) -> list:
-
+def get_urls(df_in: pd.DataFrame) -> list:
     urls_list = []
     for url in df_in.Url:
         urls_list.append(url)
 
     return urls_list
-    
+
 
 def check_url(url: str):
-
     hostname = get_hostname_from_url(url)
     headers = {
-        'Host': hostname,
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-        'Accept-Language': 'en-GB,en;q=0.5',
-        'Referer': hostname,
-        'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'same-origin'
+        "Host": hostname,
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-GB,en;q=0.5",
+        "Referer": hostname,
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "same-origin",
     }
 
     try:
@@ -66,11 +71,12 @@ def check_url(url: str):
         return url, r.status_code
 
     except ConnectionError:
-        return url,'ConnectionError'
+        return url, "ConnectionError"
     except TooManyRedirects:
-        return url,'TooManyRedirectsError'
+        return url, "TooManyRedirectsError"
     except ReadTimeout:
-        return url,'ReadTimeoutError'
+        return url, "ReadTimeoutError"
+
 
 def get_hostname_from_url(url: str) -> str:
     """Returns hostname from a url e.g pure.qub.ac.uk
@@ -84,8 +90,9 @@ def get_hostname_from_url(url: str) -> str:
     try:
         hostname = parse.urlsplit(url).hostname
     except AttributeError:
-        hostname = 'error'
+        hostname = "error"
     return hostname
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
