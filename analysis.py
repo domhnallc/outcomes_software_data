cd8e726fd03f6d5cf03ad48fea6f8345de63203f
domhnallc <d.carlin@qub.ac.uk>
2023-07-26 09:52:01 +0100

    integrate sware os? analysis

--- analysis.py
+++ analysis.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import pandas as pd
+import matplotlib.pyplot as plt
 
 '''Helper functions'''
 
@@ -16,13 +18,38 @@ def filter_dataframe(df_in: pd.DataFrame) -> pd.DataFrame:
     return filtered_df
 
 
+def csv_to_df(csv_in: str) -> pd.DataFrame:
+
+    df = pd.read_csv(csv_in, index_col=[1])
+    #print(df[''])
+
+    return df
+
+#Software Open Sourced?	Year Produced		Url
+
+def chart_data(data_df: pd.DataFrame):
+    slice_data_df = data_df[['Software Open Sourced?','Year Produced','Url']]
+    slice_data_df.plot(kind="bar")
+    plt.show()
+    print(slice_data_df)
+    num_rows = len(slice_data_df.index)
+    print("xx", num_rows - slice_data_df['Software Open Sourced?'].value_counts())
+    print("Total:", num_rows)
+    print("Software Open Sourced? ", slice_data_df['Software Open Sourced?'].value_counts(), "No:")
+    print("URL ", slice_data_df.count())
+
+
+
 def main():
 
-    df_all_data = get_dataframe_from_csv('./data/responses.csv')
+    df_responses = get_dataframe_from_csv('./data/responses.csv')
 
 
     # Get count of each http response code
-    print(df_all_data.groupby(1).count())
+    print(df_responses.groupby(1).count())
+
+    df_all_data = csv_to_df("data/gtr_raw_data.csv")
+    chart_data(df_all_data)
 
 if __name__ == '__main__':
     main()
