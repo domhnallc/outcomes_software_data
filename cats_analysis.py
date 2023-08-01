cb7e2bfa195f290830047f770990e01a07e6036f
domhnallc <d.carlin@qub.ac.uk>
2023-08-01 16:31:36 +0100

    pie chart added

--- cats_analysis.py
+++ cats_analysis.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-cols = ["url", "cat", "percentage"]
+cols = ["url", "category"]
 
 with open("./cats.csv") as data_in:
     df = pd.read_csv(data_in, names=cols, index_col="url").fillna("missing")
 
 
-    df_grouped_cat_count = df.groupby(["cat"]).size().to_frame("dfcount").reset_index("cat").sort_values(by="dfcount", ascending=False)
+    df_grouped_cat_count = df.groupby(["category"]).size().to_frame("dfcount").sort_values(by="dfcount", ascending=False)
+    df_grouped_cat_count.reset_index("category")
     
-
-    print(df_grouped_cat_count)
+    df_grouped_cat_count.plot.pie(y="dfcount", title="Title", legend=False, autopct='%1.1f%%', startangle=0)
+    #print(df_grouped_cat_count)
+    plt.show()
 
